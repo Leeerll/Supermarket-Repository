b670a9c878a14791139cee53cca32eb1fbd8ffb1
dangtianle <leeer228@163.com>
2023-04-14 22:31:01 +0800

    dtl update inputService

--- src/main/java/com/example/auto_warehouse/service/InputService.java
+++ src/main/java/com/example/auto_warehouse/service/InputService.java
@@ -1,9 +1,13 @@
 package com.example.auto_warehouse.service;
 
+import com.example.auto_warehouse.bean.Repository;
+import com.example.auto_warehouse.mapper.CargoMapper;
 import com.example.auto_warehouse.mapper.CargoStatusMapper;
+import com.example.auto_warehouse.mapper.SpeciesMapper;
 import com.example.auto_warehouse.mapper.SupermarketMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,26 +16,52 @@ public class InputService {
 
     @Autowired
     private SupermarketMapper supermarketMapper;
+    @Autowired
+    private SpeciesMapper speciesMapper;
+    @Autowired
+    private CargoMapper cargoMapper;
+//    @Autowired
+//    private CellMapper cellMapper;
+//    @Autowired
+//    private RepositoryMapper repositoryMapper;
+//    @Autowired
+//    private SaveMapper saveMapper;
+//    @Autowired
+//    private LogMapper logMapper;
 
-    public boolean check(List<Map<String,String>> data){
-        // 检查体积
-        if(Double.parseDouble(data.get(5).get("sh"))>80 || Double.parseDouble(data.get(6).get("sw"))>70 || Double.parseDouble(data.get(7).get("sd"))>60){
-            Map<String, String> reason = new HashMap<>();
-            reason.put("reason","体积过大");
-            data.add(reason);
-            notInput(data);
-            return false;
+    public void check(List<Map<String,String>> data){
+        // 不能入库的数据
+        List<Map<String,String>> notInputData = new ArrayList<>();
+        // 能入库的数据
+        List<Map<String,String>> inputData = new ArrayList<>();
+        // 遍历data
+        for(Map map:data){
+            // 检查体积
+            if(Double.parseDouble((String) map.get("sh"))>80 || Double.parseDouble((String) map.get("sw"))>70 || Double.parseDouble((String) map.get("sd"))>60){
+                map.put("reason","体积过大");
+                notInputData.add(map);
+            }
+            // 检查重量
+            else if(Double.parseDouble((String) map.get("weight"))>60){
+                map.put("reason","超重");
+                notInputData.add(map);
+            }
+            // 检查超市是否存在
+            else if(supermarketMapper.findById("10")==null){
+                map.put("reason","超市不存在");
+                notInputData.add(map);
+            }else {
+                inputData.add(map);
+            }
         }
-        //检查重量
-        if(Double.parseDouble(data.get(4).get("weight"))>60){
-            Map<String, String> reason = new HashMap<>();
-            reason.put("reason","超重");
-            data.add(reason);
-            notInput(data);
-            return false;
+        // 针对不能入库的货物
+        if(notInputData.size()>0){
+            notInput(notInputData);
         }
-
-        return true;
+        // 针对能入库的货物
+        if(inputData.size()>0){
+            callInput(inputData);
+        }
     }
 
     // 不能入库，写入停滞区
@@ -41,6 +71,10 @@ public class InputService {
     public boolean notInput(List<Map<String,String>> list){
         return false;
 
+
+    }
+
+    public void callInput(List<Map<String,String>> data){
 
     }
 
