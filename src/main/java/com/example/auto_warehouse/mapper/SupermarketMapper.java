a9c2c408332da1f2376ed03107c1bba8a4c97b51
zjq <726532751@qq.com>
2023-04-14 21:47:37 +0800

    11

--- src/main/java/com/example/auto_warehouse/mapper/SupermarketMapper.java
+++ src/main/java/com/example/auto_warehouse/mapper/SupermarketMapper.java
@@ -6,5 +6,5 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface SupermarketMapper {
-    Supermarket findById(String suid); //根据id查询某个用户，返回user实体类
+    Supermarket findById(String suid); //根据id查询超市，返回supermarket实体类
 }
--- src/main/java/com/example/auto_warehouse/service/InputService.java
+++ src/main/java/com/example/auto_warehouse/service/InputService.java
@@ -1,11 +1,17 @@
 package com.example.auto_warehouse.service;
 
+import com.example.auto_warehouse.mapper.CargoStatusMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import java.util.List;
 import java.util.Map;
 
 public class InputService {
 
-    // 不能入库，写入停滞区
+    @Autowired
+    private CargoStatusMapper CargoStatusMapper;
+
+    // 不能入库，写入数据库停滞区
     public boolean notInput(List<Map<String,String>> list){
         return false;
 
