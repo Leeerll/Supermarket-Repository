9f4acced3970c38f66905772c6fb5be8552784a2
zjq <726532751@qq.com>
2023-04-25 15:20:01 +0800

    写入income表

--- src/main/java/com/example/auto_warehouse/bean/Income.java
+++ src/main/java/com/example/auto_warehouse/bean/Income.java
@@ -7,10 +7,20 @@ import lombok.Data;
 @Data
 @Entity
 public class Income {
+    public Income(String repositoryId, int yearMonth, double income) {
+        this.repositoryId = repositoryId;
+        this.yearMonth = yearMonth;
+        this.income = income;
+    }
+    public Income(){
+
+    }
+
     @Id
     private String repositoryId;
     private int yearMonth;
     private double income;
     // 这个year用来传递当前年份
     private int year;
+
 }
--- src/main/java/com/example/auto_warehouse/mapper/IncomeMapper.java
+++ src/main/java/com/example/auto_warehouse/mapper/IncomeMapper.java
@@ -1,9 +1,14 @@
 package com.example.auto_warehouse.mapper;
 
+import com.example.auto_warehouse.bean.Income;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface IncomeMapper {
     // 每次生成订单后更新收入表
-    void updateIncome(int year, int month, double money);
+    void updateIncome(int yearMonth, double money, String repositoryId);
+    void insertIncome(Income income);
+    List<Income> findByYearMonth(String repositoryId, int yearMonth);
 }
