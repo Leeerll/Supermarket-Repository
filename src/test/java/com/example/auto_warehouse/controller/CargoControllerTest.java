1ec48fe5655a5718328cf8b23ce64adf23d0fbc9
bulaoheben <20301164@bjtu.edu.cn>
2023-06-05 00:43:34 +0800

    cargocontroller的测试写完

--- src/test/java/com/example/auto_warehouse/controller/CargoControllerTest.java
+++ src/test/java/com/example/auto_warehouse/controller/CargoControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CargoControllerTest {
@@ -38,7 +39,11 @@ class CargoControllerTest {
                 assertThat(true);
             }
         }
-        assertThat(false);
+        if(jsonArray.isEmpty()){
+            assertThat(true);
+        }else {
+            assertTrue(false);
+        }
 
     }
 
@@ -50,7 +55,6 @@ class CargoControllerTest {
         // 确认响应包含期望的文本内容
         assertThat(response.getBody()).isNotEmpty();
         //采用parse处理
-        System.out.println("555555555555555555555555555555555");
         System.out.println(response.getBody());
         JSONParser parser = new JSONParser();
         JSONArray jsonArray = (JSONArray) parser.parse(response.getBody());
@@ -63,6 +67,41 @@ class CargoControllerTest {
                 assertThat(true);
             }
         }
-        assertThat(false);
+        if(jsonArray.isEmpty()){
+            assertThat(true);
+        }else {
+            assertTrue(false);
+        }
     }
+    @Test
+    public void testshowExpire() throws ParseException {
+        // 发送一个 GET 请求到 "/hello" API
+        ResponseEntity<String> response = restTemplate.getForEntity("/cargo/showExpire", String.class);
+        assertThat(response.getStatusCode().value()).isEqualTo(200);
+        // 确认响应包含期望的文本内容
+        assertThat(response.getBody()).isNotEmpty();
+        System.out.println("88888888888888888888888888");
+        System.out.println(response.getBody());
+        //采用parse处理
+        System.out.println(response.getBody());
+        JSONParser parser = new JSONParser();
+        JSONArray jsonArray = (JSONArray) parser.parse(response.getBody());
+
+        String name="红牛";
+        System.out.println(jsonArray);
+        for (Object obj : jsonArray) {
+            JSONObject jsonObject = (JSONObject) obj;
+            if (jsonObject.getAsString("name") == name) {
+                assertThat(true);
+            }
+        }
+        if(jsonArray.isEmpty()){
+            assertThat(true);
+        }else {
+            assertTrue(false);
+        }
+        //assertThat(false);
+
+    }
+
 }
